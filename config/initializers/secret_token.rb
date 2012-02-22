b75e05e2fe2d1005ae99cfededd652b88641586f
Alexey Astafyev <av.astafyev@gmail.com>
2012-02-23 00:42:09 +0400

    added rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTaskManager::Application.config.secret_token = '9b29217c42306a46a993864be12f7498ea8afcf8a5632a29d9d04800d32ae46b1a9229482abdae837e6f79b3222dfea899af682118dff7c58580d31af0b92521'
